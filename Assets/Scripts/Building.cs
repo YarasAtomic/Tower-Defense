1b8cf49b47847d123ac74e2c0f122b0a7543a404
pabloqr <pabloquesadarojo02@gmail.com>
2023-11-29 13:02:15 +0100

    initial variables

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -1,9 +1,27 @@
 using UnityEngine;
-using UnityEngine.Splines;
 
 public class Building : MonoBehaviour
 {
-    public void Damage(int dmg){
+    protected int BASE_HP;
+	protected int PURCHASE_PRICE;
+	protected int MAX_SELLING_PRICE;
 
+    protected int hp;
+	protected int sellingPrice;
+
+	public void Build() {
+		
+	}
+
+	public void Sell() {
+		
+	}
+
+	public void Damage(int dmg) {
+
+    }
+
+    public void DestroyBuilding() {
+        
     }
 }
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+public class Tower : Building
+{
+	private Building building;
+
+	private static List<float> FACTOR_UPGRADE = new List<float> {1.0f, 1.2f, 1.4f};
+	private int UPGRADE_PRICE = 50;
+	private int BASE_REPAIR_RATE = 5;			// miliseconds
+	private int BASE_DAMAGE = 5;
+	private int FAVOURITE_ENEMY = -1;
+	private int FIRE_RATE = 10;					// miliseconds
+	private float BASE_SHOOTING_RADIUS = 5.0f;
+
+	private int currentUpgrade = 0;
+	private int maxHp;
+	private int repairCost;
+	private int repairRate;
+	private int damage;
+	private float shootingRadius;
+	private Enemy selectedEnemy;
+
+	public Tower(Building b) {
+		this.building = b;
+	}
+}
