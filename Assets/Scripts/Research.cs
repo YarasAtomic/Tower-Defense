714b44c9f1bbdee3813c1d77cef213563d5a638d
lgomxz <lgomez@correo.ugr.es>
2023-12-30 17:31:43 +0100

    Commit final research
    Quedan navegabilidad, logos y guardado global

--- Assets/Scripts/Research.cs
+++ Assets/Scripts/Research.cs
@@ -14,12 +14,12 @@ public class Research : MonoBehaviour
     private readonly float[] SUPPORT_POWER_FACTOR  = {1f, 1.2f, 1.4f, 1.6f};
 
     // Dynamic atributes
-    private int shooting_radius_upgrade;
-    private int speed_of_repair_upgrade;
-    private int weapons_armoring_upgrade;
-    private int refund_upgrade;
-    private int cooldown_upgrade; 
-    private int support_power_upgrade; 
+   [SerializeField] private int shooting_radius_upgrade;
+   [SerializeField] private int speed_of_repair_upgrade;
+   [SerializeField] private int weapons_armoring_upgrade;
+   [SerializeField] private int refund_upgrade;
+   [SerializeField] private int cooldown_upgrade; 
+   [SerializeField] private int support_power_upgrade; 
 
 // Atributo de prueba, para poder probar el funcionamiento de los botones
     [SerializeField] public int experience;
@@ -30,26 +30,44 @@ public class Research : MonoBehaviour
     void Start()
     {
        shooting_radius_upgrade = 0;
-       speed_of_repair_upgrade = 0;
-       weapons_armoring_upgrade = 0;
-       refund_upgrade = 0;
-       cooldown_upgrade = 0;
-       support_power_upgrade = 0;
-       experience = 50;
+       speed_of_repair_upgrade = 1;
+       weapons_armoring_upgrade = 2;
+       refund_upgrade = 3;
+       cooldown_upgrade = 1;
+       support_power_upgrade = 2;
+       experience = 100;
         
     }
 
     public int getShootingRadius(){
         return shooting_radius_upgrade;
     }
+    
+    public int getSpeedOfRepair(){
+        return speed_of_repair_upgrade;
+    }
+
+    public int getWeaponsArmoring(){
+        return weapons_armoring_upgrade;
+    }
+
+    public int getRefundForSelling(){
+        return refund_upgrade;
+    }
+
+    public int getSupportRechargeTime(){
+        return cooldown_upgrade;
+    }
+
+    public int getSupportPower(){
+        return support_power_upgrade;
+    }
 
     public int getExp(){
         return experience;
     }
 
-    public void spendExp(int exp){
-        experience -= exp;
-    }
+
     public int getPurchaseExp(int level){
         return UPGRADE_PRICES[level];
     }
@@ -62,6 +80,56 @@ public class Research : MonoBehaviour
         
         }
 
+    }
+
+    public void speedOfRepairUpgrade(){
+        if(speed_of_repair_upgrade < 3){
+            experience -= UPGRADE_PRICES[speed_of_repair_upgrade];
+            speed_of_repair_upgrade++;
+        }
+
+    }
+    public void weaponsArmoringUpgrade(){
+        if(weapons_armoring_upgrade < 3){
+            experience -= UPGRADE_PRICES[weapons_armoring_upgrade];
+            weapons_armoring_upgrade++;
+        }
+
+    }
+
+    public void refundForSellingUpgrade(){
+        if(refund_upgrade < 3){
+            experience -= UPGRADE_PRICES[refund_upgrade];
+            refund_upgrade++;
+        }
+
+    }
+
+    public void supportRechargeTimeUpgrade(){
+        if(cooldown_upgrade < 3){
+            experience -= UPGRADE_PRICES[cooldown_upgrade];
+            cooldown_upgrade++;
+        }
+    }
+
+    public void supportPowerUpgrade(){
+        if(support_power_upgrade < 3){
+            experience -= UPGRADE_PRICES[support_power_upgrade];
+            support_power_upgrade++;
+        }
+    }
+
+    public void updateStatus(int expTemp, int shootingRadiusTemp, int speedOfRepairTemp, int weaponsArmoringTemp, 
+                            int refundTemp, int cooldownTemp, int supportTowerTemp){
+    
+        experience = expTemp;
+        shooting_radius_upgrade = shootingRadiusTemp;
+        speed_of_repair_upgrade = speedOfRepairTemp;
+        weapons_armoring_upgrade = weapons_armoring_upgrade;
+        refund_upgrade = refundTemp;
+        cooldown_upgrade = cooldownTemp;
+        support_power_upgrade = supportTowerTemp;
+        
     }
 
     
--- Assets/Scripts/ResearchCanvas.cs
+++ Assets/Scripts/ResearchCanvas.cs
@@ -26,7 +26,17 @@ public class ResearchCanvas : MonoBehaviour
     private Color bloquedColor;
     private Color purchasedColor;
     private Color availableColor;
+    private Color newPurchaseColor;
 
+// Atributos temp
+    private int exp_temp;
+    private int shootingRadiusTemp; 
+    private int speedOfRepairTemp;
+    private int weaponsArmoringTemp;
+    private int refundTemp;
+    private int cooldownTemp;
+    private int supportPowerTemp;
+    
 
     //!---------------------------------------------------------------!//
     //!---------------------------------------------------------------!//
@@ -48,115 +58,310 @@ public class ResearchCanvas : MonoBehaviour
 
 
     void Start(){
-      //doneButton =  transform.Find("DoneButton").gameObject;
       research = researchGameObject.GetComponent<Research>();
-      //Debug.Log(doneButton);
-      //doneButtonTMP =  transform.Find("Panel/doneButton/doneButtonTMP").gameObject;
+      
+      exp_temp = research.getExp();
+      shootingRadiusTemp = research.getShootingRadius();
+      speedOfRepairTemp = research.getSpeedOfRepair();
+      weaponsArmoringTemp = research.getWeaponsArmoring();
+      refundTemp = research.getRefundForSelling();
+      cooldownTemp = research.getSupportRechargeTime();
+      supportPowerTemp = research.getSupportPower();
+    
+      bloquedColor = new Color(0.66f, 0.66f, 0.66f); // No puedo hacer nada con este botón
+      purchasedColor = new Color (0.08f,0.26f, 0.53f); // Ya he comprado esta mejora
+      availableColor = new Color(0.94f,0.94f,0.11f); // puedo comprar esta mejora
+      newPurchaseColor = new Color(0.58f, 0f, 1f);
+      //setShootingRadiusColors();
+      setAllButtonsColors();
 
-      // TODO : Poner los colores bonitos
-      bloquedColor = Color.black; // No puedo hacer nada con este botón
-      purchasedColor = Color.blue; // Ya he comprado esta mejora
-      availableColor = Color.red; // puedo comprar esta mejora
-      setShootingRadiusColors();
     }
 
+    // TODO : hacer función que cambie la exp y el estado de los f
     void Update(){
-        
+        if(exp_temp < research.getPurchaseExp(0)){
+          setAllButtonsColors();
+        }
     }
 
     public void HandleDoneButton() {
+      // TODO : navegacion
+      research.updateStatus(exp_temp, shootingRadiusTemp, speedOfRepairTemp, weaponsArmoringTemp,
+                             refundTemp, cooldownTemp, supportPowerTemp);
       
-      Debug.Log(doneButton);
-        
+      
+      setAllButtonsColors();
     }
 
-
-    
-    public void HandleShootingRadius() {
-     int total_exp = research.getExp();
-     int purchase_exp = research.getPurchaseExp(research.getShootingRadius());
-     // Aquí lo único que hacemos es comprobar si podemos comprarlo según la experiencia, el quitar la experiencia y aumentar el upgrade se hace en 
-     // research
-      if(total_exp >= purchase_exp){
-        research.shootingRadiusUpgrade();
-      }
-      setShootingRadiusColors(); // Cambiamos los colores una vez que se haya comprado algo
-      
+    public void HandleCancelButton(){
+      // TODO : navegacion 
     }
-    
 
-public void setShootingRadiusColors(){
+    //*---------------------------------------------------------------*//
+    //*---------------------- BUTTONS COLORS -------------------------*//
+    //*---------------------------------------------------------------*//
+
+
+    public void setAllButtonsColors(){
+      // Colores shootingRadius
       GameObject button_1 =  transform.Find("shootingButtons/shootingRadiusButton_1").gameObject;
       GameObject button_2 =  transform.Find("shootingButtons/shootingRadiusButton_2").gameObject;
       GameObject button_3 =  transform.Find("shootingButtons/shootingRadiusButton_3").gameObject;
- 
-      int factor = research.getShootingRadius();
-      int exp = research.getExp();
+      setButtonsColors(button_1,button_2, button_3, shootingRadiusTemp, research.getShootingRadius());
+
+      // Colores speedOfRepair
+      button_1 =  transform.Find("speedOfRepairButtons/speedOfRepairButton_1").gameObject;
+      button_2 =  transform.Find("speedOfRepairButtons/speedOfRepairButton_2").gameObject;
+      button_3 =  transform.Find("speedOfRepairButtons/speedOfRepairButton_3").gameObject;
+      setButtonsColors(button_1,button_2, button_3, speedOfRepairTemp, research.getSpeedOfRepair());
+    
+      // Colores weaponsArmoring
+      button_1 =  transform.Find("weaponsArmoringButtons/weaponsArmoringButton_1").gameObject;
+      button_2 =  transform.Find("weaponsArmoringButtons/weaponsArmoringButton_2").gameObject;
+      button_3 =  transform.Find("weaponsArmoringButtons/weaponsArmoringButton_3").gameObject;
+      setButtonsColors(button_1,button_2, button_3,weaponsArmoringTemp ,research.getWeaponsArmoring());
+
+      // Colores refundForSelling
+      button_1 =  transform.Find("refundForSellingButtons/refundForSellingButton_1").gameObject;
+      button_2 =  transform.Find("refundForSellingButtons/refundForSellingButton_2").gameObject;
+      button_3 =  transform.Find("refundForSellingButtons/refundForSellingButton_3").gameObject;
+      setButtonsColors(button_1,button_2, button_3, refundTemp, research.getRefundForSelling());
+
+      // Colores supportRecharge
+      button_1 =  transform.Find("supportRechargeButtons/supportRechargeButton_1").gameObject;
+      button_2 =  transform.Find("supportRechargeButtons/supportRechargeButton_2").gameObject;
+      button_3 =  transform.Find("supportRechargeButtons/supportRechargeButton_3").gameObject;
+      setButtonsColors(button_1,button_2, button_3,cooldownTemp, research.getSupportRechargeTime());
+
+      // Colores supportPower
+      button_1 =  transform.Find("supportPowerButtons/supportPowerButton_1").gameObject;
+      button_2 =  transform.Find("supportPowerButtons/supportPowerButton_2").gameObject;
+      button_3 =  transform.Find("supportPowerButtons/supportPowerButton_3").gameObject;
+      setButtonsColors(button_1,button_2, button_3, supportPowerTemp , research.getSupportPower());
+    }
+    
+    public void setButtonsColors( GameObject button_1, GameObject  button_2, GameObject button_3, int factor, int factor_real){
+
+      // Poner color comprado a los que tengamos ya comprados de antes
       var colors = button_1.GetComponent<Button>().colors; // TODO : Esto habria que cambiarlo pero no se como porque da error
+      colors.disabledColor = purchasedColor;
+      switch(factor_real){
+        case 1:
+          button_1.GetComponent<Button>().colors = colors;
+          button_1.GetComponent<Button>().interactable = false;
+        break;
+        case 2:
+          button_1.GetComponent<Button>().colors = colors;
+          button_1.GetComponent<Button>().interactable = false;
+          button_2.GetComponent<Button>().colors = colors;
+          button_2.GetComponent<Button>().interactable = false;
+        break;
+        case 3:
+          button_1.GetComponent<Button>().colors = colors;
+          button_1.GetComponent<Button>().interactable = false;
+          button_2.GetComponent<Button>().colors = colors;
+          button_2.GetComponent<Button>().interactable = false;
+          button_3.GetComponent<Button>().colors = colors;
+          button_3.GetComponent<Button>().interactable = false;
+        break;
+      }
+
+      int exp = research.getExp();
       switch (factor){
 
         // Todavia no hay nada comprado
         case 0: 
-            if(exp >= research.getPurchaseExp(0)){
+          if(exp_temp >= research.getPurchaseExp(0)){
+            colors.normalColor = availableColor;
+          }else
+            colors.normalColor = bloquedColor;
+
+            colors.disabledColor = bloquedColor;
+
+            button_1.GetComponent<Button>().colors = colors; // Este puedo comprarlo o no comprarlo dependiendo de la exp
+            button_2.GetComponent<Button>().colors = colors; // Queda bloqueado
+            button_3.GetComponent<Button>().colors = colors; // Queda bloqueado
+
+            button_2.GetComponent<Button>().interactable = false; // No puedo interaccionar con el
+            button_3.GetComponent<Button>().interactable = false; // No puedo interaccionar con el
+          break;
+
+          // Primer nivel comprado, puedo comprar el 2, 1 y 3 no puedo interactuar
+          case 1: 
+
+            button_2.GetComponent<Button>().interactable = true; // Ahora puedo interaccionar con este
+          
+            if(exp_temp >= research.getPurchaseExp(1)){ // Puede que tenga suficiente exp o puede que no
               colors.normalColor = availableColor;
             }else
               colors.normalColor = bloquedColor;
 
-              colors.disabledColor = bloquedColor;
+            button_2.GetComponent<Button>().colors = colors; // Actualizo su color
+            
+            if(factor_real != 1){
+              colors.disabledColor = newPurchaseColor; // Cambio el color a 1 a ya comprado
+              button_1.GetComponent<Button>().interactable = false; // No puedo comprar el 1 porque esta ya comprado
+              button_1.GetComponent<Button>().colors = colors;
+            }
+            
+            colors.disabledColor = bloquedColor;
+            button_3.GetComponent<Button>().interactable = false; // No puedo comprar el 1 porque esta ya comprado
+            button_3.GetComponent<Button>().colors = colors;
 
-              button_1.GetComponent<Button>().colors = colors; // Este puedo comprarlo o no comprarlo dependiendo de la exp
-              button_2.GetComponent<Button>().colors = colors; // Queda bloqueado
-              button_3.GetComponent<Button>().colors = colors; // Queda bloqueado
+          break;
 
-              button_2.GetComponent<Button>().interactable = false; // No puedo interaccionar con el
-              button_3.GetComponent<Button>().interactable = false; // No puedo interaccionar con el
-        break;
+          //Segundo nivel comprado
+          case 2:
+            button_3.GetComponent<Button>().interactable = true; // Ahora puedo interaccionar con este
+            if(exp_temp >= research.getPurchaseExp(2)){
+              colors.normalColor = availableColor;
+            }else
+              colors.normalColor = bloquedColor;
 
-        // Primer nivel comprado, puedo comprar el 2, 1 y 3 no puedo interactuar
-        case 1: 
+                        
+            button_3.GetComponent<Button>().colors = colors;
 
-          button_2.GetComponent<Button>().interactable = true; // Ahora puedo interaccionar con este
-          if(exp >= research.getPurchaseExp(1)){ // Puede que tenga suficiente exp o puede que no
-            colors.normalColor = availableColor;
-          }else
-            colors.normalColor = bloquedColor;
-
-          button_2.GetComponent<Button>().colors = colors; // Actualizo su color
+            if( factor_real != 2) {
+              colors.disabledColor = newPurchaseColor; // Cambio el color a 2 a ya comprado
+              button_2.GetComponent<Button>().colors = colors;
+              button_2.GetComponent<Button>().interactable = false;
+            }
+            
+          break;
           
-          colors.disabledColor = purchasedColor; // Cambio el color a 1 a ya comprado
-          button_1.GetComponent<Button>().interactable = false; // No puedo comprar el 1 porque esta ya comprado
-          button_1.GetComponent<Button>().colors = colors;
+          // Tercer nivel comprado
+          case 3: 
 
-        break;
+            if(factor_real != 3){
+              colors.disabledColor = newPurchaseColor; // Cambio el color a 3 a ya comprado
 
-        //Segundo nivel comprado
-        case 2:
-          button_3.GetComponent<Button>().interactable = true; // Ahora puedo interaccionar con este
-          if(exp >= research.getPurchaseExp(2)){
-            colors.normalColor = availableColor;
-          }else
-            colors.normalColor = bloquedColor;
+              button_3.GetComponent<Button>().interactable = false;
+              button_3.GetComponent<Button>().colors = colors;
+            }
+            
 
-          colors.disabledColor = purchasedColor; // Cambio el color a 2 a ya comprado
-          button_2.GetComponent<Button>().colors = colors;
-          button_3.GetComponent<Button>().colors = colors;
-          
-          button_2.GetComponent<Button>().interactable = false;
-        break;
+          break;
+        }
+        GameObject research_textGameObject = transform.Find("experience/experienceText").gameObject;
         
-        // Tercer nivel comprado
-        case 3: 
-          colors.disabledColor = purchasedColor; // Cambio el color a 3 a ya comprado
+        research_textGameObject.GetComponent<TMP_Text>().text = exp_temp + " XP";
+        
+     }
 
-          button_3.GetComponent<Button>().interactable = false;
-          button_3.GetComponent<Button>().colors = colors;
+    //*---------------------------------------------------------------*//
+    //*---------------------- ACTION BUTTONS -------------------------*//
+    //*---------------------------------------------------------------*//
 
-        break;
+    public void HandleShootingRadius() {
+      int purchase_exp = research.getPurchaseExp(shootingRadiusTemp);
+      // Aquí lo único que hacemos es comprobar si podemos comprarlo según la experiencia, el quitar la experiencia y aumentar el upgrade se hace en 
+      // research
+      if(exp_temp >= purchase_exp){
+        shootingRadiusTemp++;
+        exp_temp -= purchase_exp;
+      }
+
+      GameObject button_1 =  transform.Find("shootingButtons/shootingRadiusButton_1").gameObject;
+      GameObject button_2 =  transform.Find("shootingButtons/shootingRadiusButton_2").gameObject;
+      GameObject button_3 =  transform.Find("shootingButtons/shootingRadiusButton_3").gameObject;
+      setButtonsColors(button_1,button_2, button_3, shootingRadiusTemp, research.getShootingRadius()); // Cambiamos los colores una vez que se haya comprado algo
+      
+
+    }
+    
+
+    public void HandleSpeedOfRepair() {
+      int purchase_exp = research.getPurchaseExp(speedOfRepairTemp);
+      // Aquí lo único que hacemos es comprobar si podemos comprarlo según la experiencia, el quitar la experiencia y aumentar el upgrade se hace en 
+      // research
+      if(exp_temp >= purchase_exp){
+        speedOfRepairTemp++;
+        exp_temp -= purchase_exp;
       }
       
-       
+
+      GameObject button_1 =  transform.Find("speedOfRepairButtons/speedOfRepairButton_1").gameObject;
+      GameObject button_2 =  transform.Find("speedOfRepairButtons/speedOfRepairButton_2").gameObject;
+      GameObject button_3 =  transform.Find("speedOfRepairButtons/speedOfRepairButton_3").gameObject;
+      
+      setButtonsColors(button_1,button_2, button_3, speedOfRepairTemp, research.getSpeedOfRepair());
+     
+      
     }
 
+     public void HandleWeaponsArmoring() {
+      int purchase_exp = research.getPurchaseExp(weaponsArmoringTemp);
+      // Aquí lo único que hacemos es comprobar si podemos comprarlo según la experiencia, el quitar la experiencia y aumentar el upgrade se hace en 
+      // research
+      if(exp_temp >= purchase_exp){
+        weaponsArmoringTemp++;
+        exp_temp -= purchase_exp;
+      }
+
+      GameObject button_1 =  transform.Find("weaponsArmoringButtons/weaponsArmoringButton_1").gameObject;
+      GameObject button_2 =  transform.Find("weaponsArmoringButtons/weaponsArmoringButton_2").gameObject;
+      GameObject button_3 =  transform.Find("weaponsArmoringButtons/weaponsArmoringButton_3").gameObject;
+      
+      setButtonsColors(button_1,button_2, button_3, weaponsArmoringTemp, research.getWeaponsArmoring());
+     
+      
+    }
+
+    public void HandleRefundForSelling() {
+      int purchase_exp = research.getPurchaseExp(refundTemp);
+      // Aquí lo único que hacemos es comprobar si podemos comprarlo según la experiencia, el quitar la experiencia y aumentar el upgrade se hace en 
+      // research
+      if(exp_temp >= purchase_exp){
+        refundTemp++;
+        exp_temp -= purchase_exp;
+      }
+      
+      int factor = research.getRefundForSelling();
+
+      GameObject button_1 =  transform.Find("refundForSellingButtons/refundForSellingButton_1").gameObject;
+      GameObject button_2 =  transform.Find("refundForSellingButtons/refundForSellingButton_2").gameObject;
+      GameObject button_3 =  transform.Find("refundForSellingButtons/refundForSellingButton_3").gameObject;
+      
+      setButtonsColors(button_1,button_2, button_3, refundTemp, research.getRefundForSelling());
+     
+      
+    }
+
+    public void HandleSupportRechargeTime() {
+      int purchase_exp = research.getPurchaseExp(cooldownTemp);
+      // Aquí lo único que hacemos es comprobar si podemos comprarlo según la experiencia, el quitar la experiencia y aumentar el upgrade se hace en 
+      // research
+      if(exp_temp >= purchase_exp){
+        cooldownTemp++;
+        exp_temp -= purchase_exp;
+      }
+      
+      int factor = research.getSupportRechargeTime();
+
+      GameObject button_1 =  transform.Find("supportRechargeButtons/supportRechargeButton_1").gameObject;
+      GameObject button_2 =  transform.Find("supportRechargeButtons/supportRechargeButton_2").gameObject;
+      GameObject button_3 =  transform.Find("supportRechargeButtons/supportRechargeButton_3").gameObject;
+      
+      setButtonsColors(button_1,button_2, button_3, cooldownTemp, research.getSupportRechargeTime());
+    }
+
+    public void HandleSupportPower() {
+      int purchase_exp = research.getPurchaseExp(supportPowerTemp);
+      // Aquí lo único que hacemos es comprobar si podemos comprarlo según la experiencia, el quitar la experiencia y aumentar el upgrade se hace en 
+      // research
+      if(exp_temp >= purchase_exp){
+        supportPowerTemp++;
+        exp_temp -= purchase_exp;
+      }
+
+      GameObject button_1 =  transform.Find("supportPowerButtons/supportPowerButton_1").gameObject;
+      GameObject button_2 =  transform.Find("supportPowerButtons/supportPowerButton_2").gameObject;
+      GameObject button_3 =  transform.Find("supportPowerButtons/supportPowerButton_3").gameObject;
+      
+      setButtonsColors(button_1,button_2, button_3, supportPowerTemp, research.getSupportPower());
+     
+      
+    }
 
 }
 
